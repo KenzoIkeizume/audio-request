ff2463b1a4cb883b759662f487fd481d1b879109
kenzoikeizume <kenzoikeizume@hotmail.com>
2019-05-20 17:12:20 -0300

    Add a audio listener

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import { KeyboardVoice } from '@material-ui/icons';
 import Grid from '@material-ui/core/Grid';
+import ReactAudioPlayer from 'react-audio-player';
 
 import './AudioRecorder.css';
 
@@ -13,17 +14,19 @@ class AudioRecorder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      record: false
-    }
+      record: false,
+      blobURL: ''
+    };
 
     this.recordingAudio = this.recordingAudio.bind(this);
+    this.onStop = this.onStop.bind(this);
   }
 
   recordingAudio = () => {
     this.setState({
       record: !this.state.record
     });
-  }
+  };
 
   onData(recordedBlob) {
     console.log('chunk of real-time data is: ', recordedBlob);
@@ -31,11 +34,14 @@ class AudioRecorder extends React.Component {
 
   onStop(recordedBlob) {
     console.log('recordedBlob is: ', recordedBlob);
+    this.setState({
+      blobURL: recordedBlob.blobURL
+    });
   }
 
   render() {
     const { classes } = this.props;
-    
+    console.log('this.test  :', this.state.blobURL);
     return (
       <React.Fragment>
         <Grid item xs={9} md={10}>
@@ -45,13 +51,20 @@ class AudioRecorder extends React.Component {
             onStop={this.onStop}
             onData={this.onData}
             strokeColor="#3f51b5"
-            backgroundColor="#FFFFFF" />
+            backgroundColor="#FFFFFF"
+          />
         </Grid>
         <Grid item xs={3} md={2}>
-          <Fab color={this.state.record ? "secondary" : "primary"} aria-label="Add" className={classes.fab} onClick={this.recordingAudio}>
+          <Fab
+            color={this.state.record ? 'secondary' : 'primary'}
+            aria-label="Add"
+            className={classes.fab}
+            onClick={this.recordingAudio}
+          >
             <KeyboardVoice />
           </Fab>
         </Grid>
+        <ReactAudioPlayer src={this.state.blobURL} autoPlay controls />
       </React.Fragment>
     );
   }
